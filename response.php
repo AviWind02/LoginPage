f3196f6c3ab97c3967034b10c343d5c4c2def78c
Avi <59033172+AviWind02@users.noreply.github.com>
2022-04-06 21:44:28 -0400

    Update response.php

--- response.php
+++ response.php
@@ -18,7 +18,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";//dont use this method if you plan on making it a public app.
   	$results = mysqli_query($conn, $query);
   	
   	if (mysqli_num_rows($results) == 1) {
