e94b295ad1baa24a2fe3092820c4f925958e0608
AviWind02 <gillavi2002@yahoo.com>
2022-04-06 21:28:26 -0400

    Uploaded

---
+++ LoginPage/LoginWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginPage
+{
+    /// <summary>
+    //Made by Aviraj
+    //wanted to test my skills doing a log in page.
+    /// </summary>//
+    public partial class MainWindow : Window
+    {
+        network Network = new network();
+  
+        public MainWindow()
+        {
+            InitializeComponent();
+            setNews();
+        }
+
+        private async void setNews()
+        {
+            TextRange newsBox = new TextRange(NewsBox.Document.ContentEnd, NewsBox.Document.ContentEnd);
+            string respone = await Network.postRespone("https://horizon.bond/Forms/News.php");
+            newsBox.Text = respone;
+        }
+    
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+     
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernamebox.Text, password = passwordBox.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+
+
+                bool Auth = await Network.postResponeAuth(username, password);
+
+                if(Auth)
+                {
+                    MessageBox.Show("Logged in!");
+                }
+                else
+                    MessageBox.Show("username or password input not right.");
+
+            }
+            else
+                MessageBox.Show("Username or password box not filled.");
+        }
+
+        private void NewsBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LoginPage/network.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    internal class network
+    {
+        public  async Task<string> postRespone(string URL, string var = "null", string value = "null")
+        {
+            var values = new Dictionary<string, string>
+            {
+                {var, value}
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await new HttpClient().PostAsync(URL, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+
+        public async Task<bool> postResponeAuth(string username, string password)
+        {
+            var values = new Dictionary<string, string>
+            {
+                {"login_user", "login_user" },
+                { "username", username },
+                { "password", password },
+
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await new HttpClient().PostAsync("https://horizon.bond/Forms/response.php", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString == "true";
+
+        }
+    }
+}
